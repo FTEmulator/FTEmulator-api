d8520c27c4c153b3c65ed6ca47f64e831f4928fa
alexwebdev05 <alexwebdev05@proton.me>
2025-06-11 13:07:41 +0200

    first commit

---
+++ src/main/java/com/ftemulator/FTEmulator_api/FtEmulatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ftemulator.FTEmulator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtEmulatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtEmulatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftemulator/FTEmulator_api/FtEmulatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftemulator.FTEmulator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtEmulatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
