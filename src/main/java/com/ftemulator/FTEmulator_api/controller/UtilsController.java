f3414513facc9ca343def35c212a76757abab1bc
alexwebdev05 <alexwebdev05@proton.me>
2025-06-14 21:01:22 +0200

    test11

--- src/main/java/com/ftemulator/FTEmulator_api/controller/UtilsController.java
+++ src/main/java/com/ftemulator/FTEmulator_api/controller/UtilsController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UtilsController {
     @GetMapping("/status")
     public ResponseEntity<Void> status() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
