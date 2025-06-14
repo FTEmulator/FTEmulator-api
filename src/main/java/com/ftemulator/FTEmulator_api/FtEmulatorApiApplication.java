f8873b88db976b3b4badc970cc72aee6fcd31a3f
alexwebdev05 <alexwebdev05@proton.me>
2025-06-14 12:27:26 +0200

    another fix

--- src/main/java/com/ftemulator/FTEmulator_api/FtEmulatorApiApplication.java
+++ src/main/java/com/ftemulator/FTEmulator_api/FtEmulatorApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FTEmulatorApiApplication {
+public class FtEmulatorApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FTEmulatorApiApplication.class, args);
+		SpringApplication.run(FtEmulatorApiApplication.class, args);
 	}
 
 }
